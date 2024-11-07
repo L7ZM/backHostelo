91a3dbdec11e59e32380063fb95aaf4245ad47b9
elhassanianas <elhassani.anas06@gmail.com>
2024-11-07 14:54:31 +0100

    Backend creation

---
+++ BackPFE/src/test/java/com/udev/hotel/BackPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udev.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
