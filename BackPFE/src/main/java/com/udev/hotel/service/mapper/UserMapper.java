a56ac41524ff06f2e4591327b83a62f047993bc1
elhassanianas <elhassani.anas06@gmail.com>
2024-11-16 15:48:10 +0100

    to map between user and userDTO , user to userDTO + userDTO to user

--- BackPFE/src/main/java/com/udev/hotel/service/mapper/UserMapper.java
+++ BackPFE/src/main/java/com/udev/hotel/service/mapper/UserMapper.java
@@ -1,7 +1,66 @@
 package com.udev.hotel.service.mapper;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
+import com.udev.hotel.entity.Role;
+import com.udev.hotel.entity.User;
+import com.udev.hotel.service.dto.UserDTO;
 
 @Service
 public class UserMapper {
+
+	public UserDTO userToUserDTO(User user) {
+		return new UserDTO(user);
+	}
+
+	public List<UserDTO> usersToUserDTOs(List<User> users) {
+		return users.stream().filter(Objects::nonNull).map(this::userToUserDTO).collect(Collectors.toList());
+	}
+
+	public User userDTOToUser(UserDTO userDTO) {
+		 if (userDTO == null) {
+	            return null;
+	        } else {
+	            User user = new User();
+	            user.setId(userDTO.getId());
+	            user.setNom(userDTO.getNom());
+	            user.setPrenom(userDTO.getPrenom());
+	            user.setEmail(userDTO.getEmail());
+	            user.setAdresse(userDTO.getAdresse());
+	            user.setTelephone(userDTO.getTelephone());
+	            user.setPointsFidelite(userDTO.getPointsFidelite());
+	            user.setDateNaissance(userDTO.getDateNaissance());
+	            Set<Role> roles = this.authoritiesFromStrings(userDTO.getAuthorities());
+	            if (roles != null) {
+	                user.setAuthorities(roles);
+	            }
+	            return user;
+	        }
+		
+	}
+
+	public List<User> userDTOsToUsers(List<UserDTO> userDTOs) {
+		return userDTOs.stream().filter(Objects::nonNull).map(this::userDTOToUser).collect(Collectors.toList());
+	}
+
+	public User userFromId(Long id) {
+		if (id == null) {
+			return null;
+		}
+		User user = new User();
+		user.setId(id);
+		return user;
+	}
+
+	public Set<Role> authoritiesFromStrings(Set<String> strings) {
+		return strings.stream().map(string -> {
+			Role auth = new Role();
+			auth.setName(string);
+			return auth;
+		}).collect(Collectors.toSet());
+	}
+
 }
