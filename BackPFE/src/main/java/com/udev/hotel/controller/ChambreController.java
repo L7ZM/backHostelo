f67adb1d81dc67ca5516c8a95010d7d09131b5cc
elhassanianas <elhassani.anas06@gmail.com>
2024-11-26 09:32:29 +0100

    add crud for room

--- BackPFE/src/main/java/com/udev/hotel/controller/ChambreController.java
+++ BackPFE/src/main/java/com/udev/hotel/controller/ChambreController.java
@@ -1,8 +1,68 @@
 package com.udev.hotel.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udev.hotel.domain.entity.Chambre;
+import com.udev.hotel.service.ChambreService;
+
+
 @RestController
+@RequestMapping("/api/chambres")
 public class ChambreController {
 
+    @Autowired
+    private ChambreService chambreService;
+
+    @GetMapping
+    public ResponseEntity<List<Chambre>> getAllChambres() {
+        List<Chambre> chambres = chambreService.getAllChambres();
+        return new ResponseEntity<>(chambres, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Chambre> getChambreById(@PathVariable Long id) {
+        return chambreService.getChambreById(id)
+                .map(chambre -> new ResponseEntity<>(chambre, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Chambre> createChambre(@RequestBody Chambre chambre) {
+        Chambre newChambre = chambreService.createChambre(chambre);
+        return new ResponseEntity<>(newChambre, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Chambre> updateChambre(@PathVariable Long id, @RequestBody Chambre updatedChambre) {
+        try {
+            Chambre updated = chambreService.updateChambre(id, updatedChambre);
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteChambre(@PathVariable Long id) {
+        chambreService.deleteChambre(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/disponibles")
+    public ResponseEntity<List<Chambre>> getAvailableChambres() {
+        List<Chambre> chambres = chambreService.getAvailableChambres();
+        return new ResponseEntity<>(chambres, HttpStatus.OK);
+    }
 }
